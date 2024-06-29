ed2a043b551d5eef97a01b5eb660ed47fb4f7a98
Yuriy <angkira@yandex.ru>
2024-06-29 21:25:17 +0200

    UI Module

---
+++ src/app/core/ui/ui.module.ts
@@ -0,0 +1,9 @@
+import { NgModule } from '@angular/core';
+import { LoaderComponent } from './loader/loader.component';
+
+@NgModule({
+  declarations: [],
+  imports: [LoaderComponent],
+  exports: [LoaderComponent],
+})
+export class UiModule {}
